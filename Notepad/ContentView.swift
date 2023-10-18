a44ad18c3a02930287db65ba6584cc47708aa744
AppRoach <codeapproach@gmail.com>
2023-10-18 14:42:47 +0200

    Initial Commit

---
+++ Notepad/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Notepad
+//
+//  Created by Vadim Zhuk on 18/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Notepad/NotepadApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotepadApp.swift
+//  Notepad
+//
+//  Created by Vadim Zhuk on 18/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct NotepadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
